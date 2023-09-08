4e5537519f4e66d1c08f9ec7c8e092021f1c4293
nguyendathung <103556785+nguyendathung@users.noreply.github.com>
2023-09-08 11:50:39 +0700

    Delete Advanced_Projcet-main directory

--- Advanced_Projcet-main/README.md
+++
@@ -1,2 +0,0 @@
-# Advance_Projcet
-This is the project i make when I was in the internship in VIT Academy
--- Advanced_Projcet-main/fe-vite/src/pages/books/books.jsx
+++
@@ -1,7 +0,0 @@
-const BookPage = () => {
-    return(
-        <>
-            Books Page
-        </>
-    )
-}
